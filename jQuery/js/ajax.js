c7bde87c717866be80d38288d7c2a26304a4b8d3
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-01-14 12:46:08 -0600

    Método Post - Ajax

--- jQuery/js/ajax.js
+++ jQuery/js/ajax.js
@@ -2,15 +2,32 @@ $(document).ready(function(){
    
     // Load - Incrustar Resultados en un html
     var datos = $('#datos');
+    var formulario = $('#formulario');
 
    // datos.load('https://reqres.in/');
 
     // Get y Post
-    $.get("https://reqres.in/api/users", {page: 2},
-        function (response) {
-            console.log(response);
-            response.data.forEach((element, index) => {
-                datos.append('<p>'+element.first_name+" "+element.last_name+'</p>')
-            });
-        });
+    // $.get("https://reqres.in/api/users", {page: 2},
+    //     function (response) {
+    //         console.log(response);
+    //         response.data.forEach((element, index) => {
+    //             datos.append('<p>'+element.first_name+" "+element.last_name+'</p>')
+    //         });
+    // });
+
+    formulario.submit(function (e) { 
+        e.preventDefault(); // Para no ser redirijidos
+        var usuario = {
+            'name': $('input[name="name"]').val(),
+            'edad': $('input[name="age"]').val()
+        }
+        console.log(usuario);
+        $.post($(this).attr('action'), usuario,
+            function (response, textStatus, jqXHR) {
+                console.log(response)
+        }).done(function() { 
+            alert('Usuario añadido correctamente');
+         });
+        return false;
+    });
 });
