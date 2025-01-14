3052efea0c87606d6a0de426e3a95596a7d2668b
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-01-14 12:29:48 -0600

    Método Get - Ajax

--- jQuery/js/ajax.js
+++ jQuery/js/ajax.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
     // Load - Incrustar Resultados en un html
     var datos = $('#datos');
 
-    datos.load('https://reqres.in/');
+   // datos.load('https://reqres.in/');
 
+    // Get y Post
+    $.get("https://reqres.in/api/users", {page: 2},
+        function (response) {
+            console.log(response);
+            response.data.forEach((element, index) => {
+                datos.append('<p>'+element.first_name+" "+element.last_name+'</p>')
+            });
+        });
 });
