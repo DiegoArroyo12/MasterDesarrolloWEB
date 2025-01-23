15e7105268d9d60ad0e7bc7202f771ed96133e77
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-01-23 11:03:00 -0600

    Hooks / Eventos de Ciclo de Vida

--- AprendiendoAngular/src/app/app.component.ts
+++ AprendiendoAngular/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Diego Arroyo';
+  public mostrarVideojuegos: boolean = true;
+
+  ocultarVideoJuegos(value: boolean){
+    this.mostrarVideojuegos = value;
+  }
 }
--- AprendiendoAngular/src/app/videojuego/videojuego.component.ts
+++ AprendiendoAngular/src/app/videojuego/videojuego.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, DoCheck, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
     selector: 'videojuego',
     templateUrl: './videojuego.component.html'
 })
-export class VideoJuegoComponent {
+export class VideoJuegoComponent implements OnInit, DoCheck, OnDestroy {
     public titulo: string;
     public listado: string;
 
@@ -12,5 +12,21 @@ export class VideoJuegoComponent {
         this.titulo = 'Componente de Videojuegos';
         this.listado = 'Listado de los juegos más populares';
         console.log('Se ha cargado el componente.')
+    }
+
+    ngOnInit(): void {
+        console.log('On Init Ejecutado!!');
+    }
+
+    ngDoCheck(): void { /* Para que revise constantemente el código */
+        console.log('DoCheck ejecutado!!');
+    }
+
+    ngOnDestroy(): void {
+        console.log('On Destroy ejecutado!!!');
+    }
+
+    cambiarTitulo(){
+        this.titulo = 'Nuevo título del componente';
     }
 }
