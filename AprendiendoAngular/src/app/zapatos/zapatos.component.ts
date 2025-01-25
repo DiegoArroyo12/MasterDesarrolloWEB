4c27e6092914348ee1114e544c8284c3cf231c46
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-01-24 21:44:27 -0600

    Directivas ngIf, ngFor y ngSwitch

--- AprendiendoAngular/src/app/zapatos/zapatos.component.ts
+++ AprendiendoAngular/src/app/zapatos/zapatos.component.ts
@@ -10,8 +10,12 @@ import { CommonModule } from "@angular/common";
 export class ZapatosComponent implements OnInit {
     public titulo: string = 'Componente de zapatos';
     public zapatos: Array<Zapato>;
+    public marcas: String[];
+    public color: string;
 
     constructor(){
+        this.color = 'yellow';
+        this.marcas = new Array;
         this.zapatos = [
             new Zapato('Air Force', 2000, 'Nike', 'Blanco', true),
             new Zapato('Clasicos', 2500, 'Puma', 'Blanco', true),
@@ -21,5 +25,16 @@ export class ZapatosComponent implements OnInit {
 
     ngOnInit(): void {
         console.log(this.zapatos);
+
+        this.getMarcas();
+    }
+
+    getMarcas() {
+        this.zapatos.forEach((zapato, index) => {
+            if (this.marcas.indexOf(zapato.marca) < 0) {
+                this.marcas.push(zapato.marca);                
+            }
+        });
+        console.log(this.marcas);
     }
 }
