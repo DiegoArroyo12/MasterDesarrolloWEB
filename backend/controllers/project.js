e6cc29765e1ff02eca39bdd0e52364d4e7cd95e3
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-08 15:02:31 -0600

    Método para guardar nuevos documentos

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Project = require('../models/project');
+
 var controller = {
     home: function(req, res) {  
         return res.status(200).send({
@@ -9,6 +11,35 @@ var controller = {
     test: function(req, res) {
         return res.status(200).send({
             message: 'Soy el método o acción test del controlador de project'
+        });
+    },
+    saveProject: function (req, res) {
+        var project = new Project();
+     
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.year;
+        project.langs = params.langs;
+        project.image = null;
+     
+        project.save().then((projectStored) => {
+          return res.status(200).send({
+            message: "Proyecto Guardado",
+            project: projectStored,
+          });
+        })
+        .catch((error) => {
+            if (!projectStored)
+              return res
+                .status(404)
+                .send({ message: "No se pudo guardar el proyecto." });
+     
+            if (error)
+              return res
+                .status(500)
+                .send({ error: "Error al guardar el proyecto." });
         });
     }
 }
