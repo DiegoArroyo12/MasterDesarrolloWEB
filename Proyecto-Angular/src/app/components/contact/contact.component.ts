14f68972a1ee74b8905d7ec197b83a9069f91854
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2025-03-16 22:37:39 -0600

    Integrar JQuery en Angular

--- Proyecto-Angular/src/app/components/contact/contact.component.ts
+++ Proyecto-Angular/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+      $("#logo").click(function(e) {
+        e.preventDefault();
+        $("header").css("background","green")
+                   .css("height","5rem");
+      });
+  }
 }
